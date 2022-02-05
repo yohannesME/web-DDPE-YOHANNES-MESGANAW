7534a3bad3bfaafd526333eaa7ede52a369cf0f1
Yohannes Mesganaw <68536682+yohannesME@users.noreply.github.com>
2022-02-05 12:54:22 -0800

    added regexp

--- Activity 7/app.js
+++ Activity 7/app.js
@@ -4,21 +4,39 @@
         this.phone = age;
         this.age = phone;
         this.introduceMySelf = () => {
+
             parent = document.getElementById('display');
             const div = document.createElement('div');
-            div.classList.add('appended');
-            div.innerHTML = `<h1> Newly Added Form </h1><h1>Full Name: ${this.fullName}</h1>
-            <h1> Age: ${this.age} </h1><h1>PhoneNo.: ${this.phone}</h1 >`;
+            div.classList.add('added');
+            div.innerHTML =
+             `<p> the information added: </p>
+             <p>Full Name: ${this.fullName}</p>
+            <p> Age: ${this.age} </p>
+            <p>PhoneNo.: ${this.phone}</p >`;
             parent.appendChild(div)
         }
 
     }
     const callBack = () => {
         var name = document.getElementById('fullname').value;
-        var fullname = document.getElementById('phone').value;
+        var phone = document.getElementById('phone').value;
         var age = document.getElementById('age').value;
+            
+        if(name == "" || phone == "" || age == ""){
+            alert("please input all the fields.")
+            return;
+        }
+        const phoneverifyreg = '/^\(?\d{3}\)?[ .-]\(?\d{3}\)?[ .-]\(?\d{4}\)?/';
+        const fullnamereg = '/^\w+[ ]\w+$/'
+        // if(!phoneverifyreg.match(phone)){
+        //     alert("please input a valid format phone number 12 digits.")
+        //     return;
+        // }
+        // if(!fullnamereg.match(name)){
+        //     alert("please input a valid full name. [firstname lastname]");
+        // }
 
-        const newObject = new Register(name, fullname, age);
+        const newObject = new Register(name, phone, age);
         newObject.introduceMySelf()
     }
     document.getElementById('btn').addEventListener('click', callBack);
